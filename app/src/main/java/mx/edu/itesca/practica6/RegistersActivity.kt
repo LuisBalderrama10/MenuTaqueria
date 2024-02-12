9f285f7a46cf51846a205a0c2026aecba99f7ec3
Multimedia <Multimedia@cajeme.edu.mx>
2024-02-12 16:45:51 -0700

    Initial commit

---
+++ app/src/main/java/mx/edu/itesca/practica6/RegistersActivity.kt
@@ -0,0 +1,11 @@
+package mx.edu.itesca.practica6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegistersActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registers)
+    }
+}
